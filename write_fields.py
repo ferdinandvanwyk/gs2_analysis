0832db9c4ac9761fc2b36adcb3ff831bcee0d714
ferdinandvanwyk <ferdinandvwyk@gmail.com>
2016-09-27 12:49:18 +0100

    add phi2 write out

--- write_fields.py
+++ write_fields.py
@@ -60,6 +60,54 @@ def write_v_exb(run):
     run.v_exb = None
     gc.collect()
 
+def write_phi2(run):
+    """
+    Write the radial ExB velocity to a NetCDF file.
+    """
+    run.read_phi()
+
+    os.system('mkdir -p ' + run.run_dir + 'analysis/write_fields')
+
+    #interpolate radial coordinate to be approx 0.5cm
+    interp_fac = int(np.ceil(run.x[int(run.nx/2)+1]/0.005))
+    x_nc = np.linspace(min(run.x), max(run.x), interp_fac*run.nx)
+    field_interp = np.empty([run.nt, len(x_nc), run.ny],
+                            dtype=float)
+    for it in range(run.nt):
+        for iy in range(run.ny):
+                f = interp.interp1d(run.x,
+                                    run.phi[it,:,iy])
+                field_interp[it,:,iy] = f(x_nc)
+
+    if run.lab_frame:
+        nc_file = Dataset(run.run_dir +
+                          'analysis/write_fields/phi_lab_frame.cdf', 'w')
+    else:
+        nc_file = Dataset(run.run_dir + 'analysis/write_fields/phi.cdf', 'w')
+
+    nc_file.createDimension('x', len(x_nc))
+    nc_file.createDimension('y', run.ny)
+    nc_file.createDimension('t', run.nt)
+    nc_file.createDimension('none', 1)
+    nc_nref = nc_file.createVariable('nref','d', ('none',))
+    nc_tref = nc_file.createVariable('tref','d', ('none',))
+    nc_x = nc_file.createVariable('x','d',('x',))
+    nc_y = nc_file.createVariable('y','d',('y',))
+    nc_t = nc_file.createVariable('t','d',('t',))
+    nc_field = nc_file.createVariable('phi2',
+                                  'd',('t', 'x', 'y'))
+
+    nc_field[:,:,:] = field_interp[:,:,:]
+    nc_nref[:] = run.nref
+    nc_tref[:] = run.tref
+    nc_x[:] = x_nc[:]
+    nc_y[:] = run.y[:]
+    nc_t[:] = run.t[:] - run.t[0]
+    nc_file.close()
+
+    run.phi = None
+    gc.collect()
+
 def write_ntot_i(run):
     """
     Write the radial ExB velocity to a NetCDF file.
@@ -113,5 +161,6 @@ run = Run(sys.argv[1])
 
 run.lab_frame = False
 write_v_exb(run)
+write_phi2(run)
 write_ntot_i(run)
 
