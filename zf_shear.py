1e60a629a7631163cfab5df25723fd6e07c4dfcc
ferdinandvanwyk <ferdinandvwyk@gmail.com>
2017-01-01 12:55:22 +0000

    add zf calc checks

--- zf_shear.py
+++ zf_shear.py
@@ -36,6 +36,34 @@ def write_zf_shear(run):
     json.dump(res, open(run.run_dir + 'analysis/zonal_flows/results.json', 'w'),
               indent=2)
 
+def check_zf_calcs(run):
+    """
+    Manually calculate gradients of phi to check fourier space calculation of
+    zonal flows.
+    """
+    run.calculate_v_zf()
+    run.calculate_zf_shear()
+
+    phi_k = field.get_field(run.cdf_file, 'phi_igomega_by_mode', None)
+
+    # Show zonal flow velocity is equivalent
+    dx = np.abs(run.x[1] - run.x[0])
+
+    phi_zf = 0.5 * run.kxfac * \
+             np.fft.ifft(phi_k[0, :, 0]).real * run.nx * run.rho_star
+    v_zf = np.gradient(phi_zf, dx)
+
+    plt.plot(run.x, run.v_zf[0,:])
+    plt.plot(run.x, v_zf)
+    plt.show()
+
+    # Show zonal shears are equivalent
+    zf_shear = np.gradient(v_zf, dx)
+
+    plt.plot(run.x, run.zf_shear[0,:])
+    plt.plot(run.x, zf_shear/run.rhoref)
+    plt.show()
+
 if __name__ == '__main__':
     run = Run(sys.argv[1])
 
